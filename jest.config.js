f89029b76acb47b4d3fb3ba4485422e4a50d160f
Genilton Barbosa <genilton.barbosa@boxti.com.br>
2022-10-10 00:17:57 -0300

    chore: fix jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,9 @@
-import type { Config } from '@jest/types'
-
-const config: Config.InitialOptions = {
+module.export = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  // testEnviroment: 'node',
+  testEnviroment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
 }
-
-export default config
